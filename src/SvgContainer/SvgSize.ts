a2ac63a21f3ba87f00aae266ba9661ca77d1772f
Khusamov Sukhrob <khusamov@yandex.ru>
2019-09-12 15:16:09 +0300

    В классе SvgSize добавлено извлечение ширины и высоты

--- src/SvgContainer/SvgSize.ts
+++ src/SvgContainer/SvgSize.ts
@@ -1,27 +1,35 @@
 import SvgUnit from './SvgUnit';
+import matchAll from '../utils/matchAll';
 
 export interface ISvgSize {
 	width: number;
 	height: number;
-	unit: string;
+	unit: string | undefined;
 }
 
 export default class SvgSize {
 	static parse(documentElement: Element): ISvgSize | undefined {
-
 		let result: ISvgSize | undefined;
-
-		const widthValue = documentElement.getAttribute('width');
-		const heigthValue = documentElement.getAttribute('heigth');
-
-		if (widthValue && heigthValue) {
+		const width = getNumberFromSize(documentElement.getAttribute('width'));
+		const height = getNumberFromSize(documentElement.getAttribute('height'));
+		if (width !== undefined && height !== undefined) {
 			result = {
-				width: 0,
-				height: 0,
+				width,
+				height,
 				unit: SvgUnit.parse(documentElement)
 			};
 		}
-
 		return result;
 	}
+}
+
+function getNumberFromSize(value: string | undefined | null): number | undefined {
+	let result;
+	if (value) {
+		const matches = matchAll(value, /^(\d+)/gm);
+		if (matches.length) {
+			result = matches[0][1];
+		}
+	}
+	return result;
 }
