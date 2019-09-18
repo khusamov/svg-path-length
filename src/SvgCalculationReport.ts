a19c33987c69e755f5d638cf9ff18b8bbfc4b75c
Khusamov Sukhrob <khusamov@yandex.ru>
2019-09-18 15:50:47 +0300

    Добавлен JSON-отчет

--- src/SvgCalculationReport.ts
+++ src/SvgCalculationReport.ts
@@ -3,12 +3,42 @@ import {ITotalLengthCalculationResult} from './SvgLengthCalculator';
 export default class SvgCalculationReport {
 	private log: string[];
 
-	constructor(totalLengthCalculationResult: ITotalLengthCalculationResult) {
-		this.createReport(totalLengthCalculationResult);
-	}
+	constructor(private totalLengthCalculationResult: ITotalLengthCalculationResult) {}
 
 	toString() {
+		this.createReport(this.totalLengthCalculationResult);
 		return this.log.join('\n');
+	}
+
+	/**
+	 * Отчет в расширенном виде в формате JSON.
+	 */
+	toJson() {
+		interface IAnyObject {[key: string]: any;}
+		const report: IAnyObject = {};
+		report.results = this.totalLengthCalculationResult.results;
+		report.total = this.withUnit(this.totalLengthCalculationResult.totalLength);
+		report.sourceSvg = this.totalLengthCalculationResult.sourceSvgContainer.svgText;
+		report.optimizedSvg = this.totalLengthCalculationResult.optimizedSvgContainer.svgText;
+		report.errors = (
+			this.totalLengthCalculationResult.results.reduce((errors: IAnyObject[], result) => {
+				if (result.hasErrors) {
+					errors.push({
+						name: result.name,
+						errors: result.parts.reduce((errors: IAnyObject[], part) => {
+							if (part.error) {
+								errors.push({
+									error: part.error,
+									markup: part.markup
+								});
+							}
+							return errors;
+						}, [])
+					});
+				}
+				return errors;
+			}, [])
+		);
 	}
 
 	private clear(): this {
@@ -26,11 +56,13 @@ export default class SvgCalculationReport {
 		return this;
 	}
 
+	private withUnit(num: number): string {
+		num = this.totalLengthCalculationResult.optimizedSvgContainer.convertCoord(num);
+		num = Math.round(num);
+		return `${num} ${this.totalLengthCalculationResult.optimizedSvgContainer.unit || ''}`;
+	}
+
 	private createReport(totalLengthCalculationResult: ITotalLengthCalculationResult) {
-		const withUnit = num => {
-			num = totalLengthCalculationResult.optimizedSvgContainer.convertCoord(num);
-			return `${num} ${totalLengthCalculationResult.optimizedSvgContainer.unit || ''}`;
-		};
 		this
 			.clear().break()
 			.add(totalLengthCalculationResult.sourceSvgContainer.svgText).break()
@@ -38,10 +70,10 @@ export default class SvgCalculationReport {
 			.add('Calculation Results:');
 		for (const result of totalLengthCalculationResult.results) {
 			if (result.length) {
-				this.add(`${result.name}: ${withUnit(result.length)} ${result.hasErrors ? 'has-errors' : ''}`);
+				this.add(`${result.name}: ${this.withUnit(result.length)} ${result.hasErrors ? 'has-errors' : ''}`);
 			}
 		}
-		this.add(`Total: ${withUnit(totalLengthCalculationResult.totalLength)}`).break();
+		this.add(`Total: ${this.withUnit(totalLengthCalculationResult.totalLength)}`).break();
 		this.createErrorReport(totalLengthCalculationResult);
 	}
 
