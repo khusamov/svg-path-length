c699692b0411b7a727c8b63a960afd5fe83fe9ca
Khusamov Sukhrob <khusamov@yandex.ru>
2019-09-23 18:20:19 +0300

    Добавлен пакет с микросервисом svg-path-length-service (с тестовым методом length)

---
+++ packages/svg-path-length-service/README.md
@@ -0,0 +1,11 @@
+# `svg-path-length-service`
+
+> TODO: description
+
+## Usage
+
+```
+const svgPathLengthService = require('svg-path-length-service');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/svg-path-length-service/src/index.ts
@@ -0,0 +1,111 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import readPackageJson from 'read-pkg';
+import koaBody from 'koa-body';
+import logger from 'koa-logger';
+import {promisify} from 'util';
+import {readFile} from 'fs';
+import {join} from "path";
+import {SvgContainer, SvgLengthCalculator, SvgCalculationReport} from 'svg-path-length-library';
+import {PathLengthPlugin, CircleLengthPlugin, EllipseLengthPlugin} from 'svg-path-length-library';
+
+(async () => {
+	const packageJson = await readPackageJson();
+	console.log(packageJson.name);
+	console.log(packageJson.description);
+	console.log(packageJson.version);
+
+	const app = new Koa;
+
+	// Обработка ошибок.
+	app.use(async (ctx, next) => {
+		try {
+			await next();
+		} catch (error) {
+			console.log(error);
+			const status = error.status || 500;
+			ctx.status = status;
+			ctx.body = {
+				status: status,
+				name: error.name,
+				message: error.message,
+				stack: error.stack.split('\n'),
+				fileName: error.fileName,
+				columnNumber: error.columnNumber,
+				lineNumber: error.lineNumber
+			};
+		}
+	});
+
+	app.use(logger());
+
+	const router = createRouter(packageJson);
+	app.use(router.routes());
+
+	app.listen(3000);
+})();
+
+/**
+ * Основной маршрутизатор.
+ * @param packageJson
+ */
+function createRouter(packageJson) {
+	const router = new Router();
+
+	/**
+	 * Главная страница.
+	 */
+	router.get('/', async ctx => {
+		ctx.body = `
+			${packageJson.name}
+			<br/>Описание: ${packageJson.description}
+			<br/>Версия: ${packageJson.version}
+		`;
+	});
+
+	/**
+	 * Метод для расчета длины.
+	 */
+	router.post('/length', koaBody({multipart: true}), async ctx => {
+		if (ctx.request.files && ctx.request.files['svg-file-content']) {
+			const svgFilePath = ctx.request.files['svg-file-content'].path;
+			const result = await calculate(svgFilePath);
+			ctx.body = {
+				success: true,
+				result: {
+					length: result.result.totalLength,
+					unit: '',
+					result: result.result,
+					report: result.report.toJson()
+				}
+			};
+		} else {
+			ctx.body = {
+				success: false,
+				message: 'Ожидается параметр svg-file-content.'
+			};
+		}
+	});
+
+	return router;
+}
+
+async function calculate(filepath) {
+	const svgContainer = await SvgContainer.createFromFile(filepath);
+	const svgLengthCalculator = (
+		new SvgLengthCalculator(svgContainer, {
+			isCalculateCirclesSeparately: true
+		})
+	);
+	const totalLengthCalculationResult = (
+		await svgLengthCalculator.calculate([
+			new PathLengthPlugin,
+			new CircleLengthPlugin,
+			new EllipseLengthPlugin
+		])
+	);
+	return {
+		result: totalLengthCalculationResult,
+		report: new SvgCalculationReport(totalLengthCalculationResult)
+	};
+}
