302f66aae1daaec376f597ef9be83a8379164175
Khusamov Sukhrob <khusamov@yandex.ru>
2019-10-14 18:32:41 +0300

    svg-path-length-service: Добавлен вывод номера порта при запуске и убран вывод имени приложения (оставлен вывод описания)

--- packages/svg-path-length-service/bin/svg-path-length-service.js
+++ packages/svg-path-length-service/bin/svg-path-length-service.js
@@ -5,8 +5,11 @@ const Path = require('path');
 const SvgPathLengthService = require('../dist').default;
 
 const packageJson = require(Path.join(__dirname, '../package.json'));
-console.log(packageJson.name);
 console.log(packageJson.description);
-console.log(packageJson.version);
+console.log('Версия:', packageJson.version);
 
-new SvgPathLengthService(packageJson).listen();
+(async function() {
+	const service = new SvgPathLengthService(packageJson);
+	await service.listen();
+	console.log('Сервис доступен по адресу:', `http://localhost:${service.port}`)
+})();
