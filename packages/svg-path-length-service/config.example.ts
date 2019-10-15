1214ce73633fc8c1a4d4115ac75b4452c0631b2c
Khusamov Sukhrob <khusamov@yandex.ru>
2019-10-15 18:48:26 +0300

    svg-path-length-service: Добавлен файл config.example.ts

---
+++ packages/svg-path-length-service/config.example.ts
@@ -0,0 +1,71 @@
+/**
+ * Пример конфигурационного файла
+ * для сервиса расчета цены лазерного реза фанеры.
+ */
+
+/**
+ * Таблица материалов с ценами.
+ * Цены представлены в таблице: Толщина (мм), Длина (мм), Цена (руб./метр).
+ */
+const materialTable = [{
+	name: 'Фанера',
+	code: 'plywood',
+	price: {
+		3: [{length: {min: 0, max: 100}, price: 24}, {length: {min: 100, max: 500}, price: 16}],
+		4: [{length: {min: 0, max: 100}, price: 29}, {length: {min: 100, max: 500}, price: 19}]
+	}
+}, {
+	name: 'Оргстекло',
+	code: 'plywood',
+	price: {
+		3: [{length: {min: 0, max: 100}, price: 24}, {length: {min: 100, max: 500}, price: 16}],
+		4: [{length: {min: 0, max: 100}, price: 29}, {length: {min: 100, max: 500}, price: 19}]
+	}
+}, {
+	name: 'МДФ (ДВП)',
+	code: 'fibreboard',
+	price: {
+		3: [{length: {min: 0, max: 100}, price: 24}, {length: {min: 100, max: 500}, price: 16}],
+		4: [{length: {min: 0, max: 100}, price: 29}, {length: {min: 100, max: 500}, price: 19}]
+	}
+}, {
+	name: 'Массив дерева',
+	code: 'solid-wood',
+	price: {
+		3: [{length: {min: 0, max: 100}, price: 24}, {length: {min: 100, max: 500}, price: 16}],
+		4: [{length: {min: 0, max: 100}, price: 29}, {length: {min: 100, max: 500}, price: 19}]
+	}
+}, {
+	name: 'Картон',
+	code: 'cardboard',
+	price: {
+		3: [{length: {min: 0, max: 100}, price: 24}, {length: {min: 100, max: 500}, price: 16}],
+		4: [{length: {min: 0, max: 100}, price: 29}, {length: {min: 100, max: 500}, price: 19}]
+	}
+}];
+
+/**
+ * Расчет цены реза.
+ * @param length Длина реза, мм.
+ * @param thickness Толщина материала.
+ * @param material Тип материала.
+ */
+export function calculatePrice(length, thickness, material) {
+	const materialData = materialTable.find(materialData => materialData.code === material);
+	if (!materialData) throw new Error(`Не найден материал '${material}'`);
+
+	const priceTable = materialData.price[thickness];
+	if (!priceTable) throw new Error(`Не найдена толщина ${thickness} для материала ${material}`);
+
+	const item = materialData.price[thickness].find(item => item.length.min >= length && item.length.max < length);
+	if (!item) throw new Error(`Не найдена цена реза длиной ${length} мм для материала ${material} (${thickness} мм)`);
+
+	return item.price * (length / 100);
+}
+
+/**
+ * Получить массив материалов с их данными.
+ */
+export function getMaterialTable() {
+	return materialTable;
+}
