ed2791020d09a4910e3d063453cb34b8200dece3
Khusamov Sukhrob <khusamov@yandex.ru>
2019-10-15 18:51:43 +0300

    svg-path-length-service: В роутере MainRouter добавлены методы GET /materials и POST /calculate-price

--- packages/svg-path-length-service/src/SvgPathLengthService/MainRouter.ts
+++ packages/svg-path-length-service/src/SvgPathLengthService/MainRouter.ts
@@ -4,6 +4,8 @@ import koaBody from 'koa-body';
 import calculateLength from './calculateLength';
 import {ISvgPathLengthServiceKoaState} from './SvgPathLengthService';
 import HttpCodeBadRequestError from './httpCode/HttpCodeBadRequestError';
+import Config from './Config';
+import {IMaterialTableItem} from '..';
 
 /**
  * Основной маршрутизатор.
@@ -20,7 +22,9 @@ export default class MainRouter {
 		const router = new Router<ISvgPathLengthServiceKoaState, ISvgPathLengthServiceKoaState>();
 		router.get('/', this.rootController);
 		router.post('/length', koaBody({multipart: true}), this.lengthController);
-		router.get('/length', this.createGetStubController());
+		router.get('/length', this.createGetStubController);
+		router.get('/materials', this.getMaterialTableController);
+		router.post('/calculate-price', koaBody({multipart: true}), this.calculatePriceController);
 		return router;
 	}
 
@@ -45,6 +49,63 @@ export default class MainRouter {
 		const svgFilePath = ctx.request.files['svg-file-content'].path;
 		const result = await calculateLength(svgFilePath);
 		ctx.body = {result};
+	};
+
+	/**
+	 * Получить таблицу с материалами.
+	 * @param ctx
+	 */
+	private static getMaterialTableController = async (ctx: Context) => {
+		ctx.body = ctx.state.service.config.getMaterialTable();
+	};
+
+	/**
+	 * Расчитать цену реза.
+	 * @param ctx
+	 */
+	private static calculatePriceController = async (ctx: Context) => {
+		const config = ctx.state.service.config as Config;
+		const materials = config.getMaterialTable();
+		let {length: lengthText, thickness: thicknessText, material: materialCode} = ctx.request.body;
+		let material: IMaterialTableItem | undefined;
+
+		if (materialCode === undefined) throw new HttpCodeBadRequestError('Ожидается параметр material');
+		material = materials.find(material => material.code === materialCode);
+		if (!material) throw new HttpCodeBadRequestError(`Материал с кодом '${materialCode}' не найден`);
+
+		const thickness = parseInt(thicknessText);
+		if (thicknessText === undefined) throw new HttpCodeBadRequestError('Ожидается параметр thickness');
+		if (isNaN(thickness)) throw new HttpCodeBadRequestError('Параметр thickness содержит не число');
+		if (!material.price[thickness]) {
+			throw new HttpCodeBadRequestError(`В таблице цен материала '${materialCode}' не найдена толщина материала ${thickness} мм'`);
+		}
+
+		const svgFileContent = (
+			ctx.request.files && ctx.request.files['svg-file-content']
+				? ctx.request.files['svg-file-content']
+				: null
+		);
+
+		if (svgFileContent && lengthText !== undefined) {
+			throw new HttpCodeBadRequestError('Параметры svg-file-content или length передавать вместе нельзя');
+		}
+
+		let length;
+		if (lengthText === undefined) {
+			if (!svgFileContent) {
+				throw new HttpCodeBadRequestError('Ожидается параметр svg-file-content или length');
+			}
+			const svgFilePath = svgFileContent.path;
+			const calculateLengthResult = await calculateLength(svgFilePath);
+			const unit = calculateLengthResult.totalLength.unit;
+			if (unit !== 'mm') throw new Error(`Единица измерения линий '${unit}', а ожидается mm`);
+			length = calculateLengthResult.totalLength.value;
+		} else {
+			length = parseInt(lengthText);
+		}
+		if (isNaN(length)) throw new HttpCodeBadRequestError('Параметр length содержит не число');
+
+		ctx.body = config.calculatePrice(length, thickness, materialCode);
 	};
 
 	/**
